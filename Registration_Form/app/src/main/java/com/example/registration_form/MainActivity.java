2e14912a0f4341a19e2209e5974df38a4baa198e
SakshiS2001 <sakshisawantj@gmail.com>
2023-10-14 20:31:49 +0530

    first commit

--- Registration_Form/app/src/main/java/com/example/registration_form/MainActivity.java
+++ Registration_Form/app/src/main/java/com/example/registration_form/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "You Have Successfully Register!", Toast.LENGTH_SHORT).show();
+
             }
         });
     }
